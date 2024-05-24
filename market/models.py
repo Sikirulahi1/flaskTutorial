8b22d774471c76ea8c962cbc505803d68529e1a1
Sikirulahi <kareemsikiru2018@gmail.com>
2024-05-24 04:34:58 +0100

    Flask tutorial continuation

--- market/models.py
+++ market/models.py
@@ -1,5 +1,6 @@
 from market import db
 from market import app
+from market import bcrypt
 
 
 
@@ -10,6 +11,20 @@ class User(db.Model):
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
+    
+    @property
+    def password(self):
+        return self.password
+    
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+        
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)  # To check is the password is correct, returns true or false
+            
+        
 
 
 class Item(db.Model):
